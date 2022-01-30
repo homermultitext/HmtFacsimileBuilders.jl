5ce92fba5ced9cce061928663db2566f51f715cb
Neel Smith <dnsmith.neel@gmail.com>
2022-01-30 16:58:37 -0500

    Set up a sample pair of codices

--- src/simplems.jl
+++ src/simplems.jl
@@ -3,3 +3,10 @@ struct SimpleMSFacsimile <: AbstractFacsimile
     hmt::Archive
     codex::Cite2Urn
 end
+
+HMT_CODICES = Dict(
+    "urn:cite2:hmt:msB.v1:" => "vbpages.cex",
+    "urn:cite2:citebl:burney86pages.v1:" => "burney86pages.cex"
+)
+
+
