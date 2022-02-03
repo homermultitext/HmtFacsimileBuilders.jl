20ec595b65ec1ffac6d83221b769815574300711
Neel Smith <dnsmith.neel@gmail.com>
2022-02-03 09:59:00 -0500

    Add basic Iliad display

--- src/stringifiediliad_mdpageformatter.jl
+++ src/stringifiediliad_mdpageformatter.jl
@@ -9,7 +9,18 @@ function stringified_iliad_mdpage(lego::StringifiedIliadLego; navigation = true)
     hdr = "# " * pagelabel(lego)
     nav = navigation ? mdnavblock(prevnext(lego)) : ""
     iltext = diplomaticiliad(lego)
-    ilhdr = isempty(iltext) ? "" : "## *Iliad* text\n\n($(length(iltext)) lines)"
+    ilhdr = isempty(iltext) ? "" : "## *Iliad* text ($(length(iltext)) lines)\n\n---"
+
+    iliadpassages = []
+    for tripl in iltext
+        
+        txtdisplay = iliadpassage(tripl)
+        xreff = haskey(lego.iliadtoscholia, tripl[1]) ?             scholialinks(lego.iliadtoscholia[tripl[1]]) : ""
+        
+
+        push!(iliadpassages, txtdisplay * "\n\n" * xreff * "\n\n---")
+
+    end
 
     othertext = diplomaticother(lego)
     otherhdr = isempty(othertext) ? "" : "## Other texts\n\n($(length(othertext)) passages)"
@@ -18,7 +29,7 @@ function stringified_iliad_mdpage(lego::StringifiedIliadLego; navigation = true)
         nav,
         thumbnail(lego),
         ilhdr,
-
+        join(iliadpassages, "\n\n"),
         otherhdr,
         
         nav
@@ -26,12 +37,36 @@ function stringified_iliad_mdpage(lego::StringifiedIliadLego; navigation = true)
 end
 
 
+function scholialinks(scholia)
+    
+    "Commented on by " * join(scholia, " ")
+end
+
+function iliadpassage(tripl::Tuple{String, String, String})
+    psg = tripl[1]
+    txt = tripl[2]
+    img = tripl[3]
+    img * "\n\n" * iliadanchor(psg) * "\n*Iliad* **" * lastcomponent(psg) * "**: " * txt 
+end
 
 
 
 
+"""Extract last colon-delimited component from a string.;
+$(SIGNATURES)
+"""
+function lastcomponent(s::AbstractString)
+    replace(s, r"[^:]*:" => "")
+end
+
+function iliadanchor(psg::AbstractString)
+    "<a name=\"il_" * lastcomponent(psg) * "\"/>"
+end
 
 
+"""Format navigation links in a markdown table.
+$(SIGNATURES)
+"""
 function mdnavblock(pr::Tuple{String, String})
     prev = pr[1]
     prevlabel = replace(replace(prev, ".md" => ""), "." => " ")
